2f4f5e9ee1aeab6fbbd84e2723c35aec01b245af
MD.JAKARIA <150427217+jakaria76@users.noreply.github.com>
2025-11-27 05:48:31 +0600

    added blood management

---
+++ ViewModels/BloodGroupVM.cs
@@ -0,0 +1,10 @@
+﻿namespace CSS.ViewModels
+{
+    public class BloodGroupVM
+    {
+        public string BloodGroup { get; set; } = default!;
+        public int Total { get; set; }
+        public int Available { get; set; }
+        public int NotAvailable { get; set; }
+    }
+}
---
+++ ViewModels/DonorWithDistance.cs
@@ -0,0 +1,10 @@
+﻿using CSS.Models;
+
+namespace CSS.ViewModels
+{
+    public class DonorWithDistance
+    {
+        public ApplicationUser User { get; set; } = default!;
+        public double DistanceKm { get; set; }
+    }
+}
