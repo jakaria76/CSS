c7c1007351c5eb0a4c8f94f8a9545fa0ab43003f
MD.JAKARIA <150427217+jakaria76@users.noreply.github.com>
2025-11-27 03:05:52 +0600

    admin manage all members

---
+++ ViewModels/AdminCommitteeVM.cs
@@ -0,0 +1,9 @@
+﻿namespace CSS.ViewModels
+{
+    public class AdminCommitteeVM
+    {
+        public string Id { get; set; }
+        public string? FullName { get; set; }
+        public string? CommitteePosition { get; set; }
+    }
+}
---
+++ ViewModels/AdminCreateUserVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSS.ViewModels
+{
+    public class AdminCreateUserVM
+    {
+        [Required] public string FullName { get; set; }
+        [Required, EmailAddress] public string Email { get; set; }
+
+        public string? Mobile { get; set; }
+        public string? AlternativeMobile { get; set; }   // NEW
+
+        public string? MemberType { get; set; }
+        public string? CommitteePosition { get; set; }
+    }
+}
